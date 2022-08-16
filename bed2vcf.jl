07ba026ccf271d82856e369066c8e4de3dd46d42
anmabu <anne.busch@uni-mainz.de>
2022-08-16 16:19:20 +0200

    tidy up

--- bed2vcf.jl
+++ bed2vcf.jl
@@ -6,17 +6,15 @@ using VariantCallFormat
 file = "/home/annebusch/Documents/anne/PhD/bedMethyl/modbam2bed-master/test_data/6mA.csv"
 content = CSV.read(file, DataFrame, delim="\t", header=false)
 
-close(content)
 
 reader = VCF.Reader(open("/home/annebusch/Documents/anne/PhD/pinf_sc50.vcf", "r"))
-
 records = read(reader)
 print(records)
-
 close(reader)
+
+
 record = VCF.Record("20\t145430\trs6054257\tG\tA\t29\tPASS\tNS=3;DP=14;AF=0.5;DB;H2\tGT:GQ:DP:HQ\t0|0:48:1:51,51\t1|0:48:8:51,51")
 VCF.pos(record)
-
 
 header = reader.header
 print(header)
