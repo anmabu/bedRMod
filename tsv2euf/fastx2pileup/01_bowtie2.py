6e99e9a782bb12afd7f5d62f368ce38d72e31273
anmabu <anne.busch@uni-mainz.de>
2023-06-02 16:07:55 +0200

    made ref_sequence flexible after accidentally hardcoding it.

--- tsv2euf/fastx2pileup/01_bowtie2.py
+++ tsv2euf/fastx2pileup/01_bowtie2.py
@@ -89,10 +89,10 @@ def mp_bowtie2(ref_sequence_dir, ref_sequence, fastq_sequence_path, output_dir, 
                    + f"{bowtie_args_filename}" + ".sam"
 
         if paired:
-            call_bowtie2("GCA_ref", fastq_sequence_path, sam_file, bowtie_args, threads=threads, paired=True,
+            call_bowtie2(ref_sequence, fastq_sequence_path, sam_file, bowtie_args, threads=threads, paired=True,
                          bowtie2_dir=bowtie2_dir)
         else:
-            call_bowtie2("GCA_ref", fastq_sequence_path, sam_file, bowtie_args, threads=threads, paired=False,
+            call_bowtie2(ref_sequence, fastq_sequence_path, sam_file, bowtie_args, threads=threads, paired=False,
                          bowtie2_dir=bowtie2_dir)
 
 
@@ -139,5 +139,5 @@ if __name__ == "__main__":
     # print(os.environ.get("BT2_HOME"))
     # mp_bowtie2(ref_sequence_path, "GCA_ref", fastq_sequence, test_output_dir, "--local", threads=3, paired=False)
     run_mp_bowtie2_dir(ref_sequence_path, "GCF_ref", fastq_input_dir, test_output_dir,
-                       bowtie_args=["--local", "-N 1", "-L 10", "--nofw", "-i S,1,0.50", "-D 20", "-R 3"],
+                       bowtie_args=["--local", "-N 1", "-L 10", "--nofw", "-D 20", "-R 3", "-k 1"],
                        threads=7, paired=False, bowtie2_dir=bowtie_dir)
