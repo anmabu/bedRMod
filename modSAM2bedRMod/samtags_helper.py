d023e686861a3f34d61dc082c3652704be102dfd
anmabu <abusch01@students.uni-mainz.de>
2024-01-05 14:30:02 +0100

    set "consecutive" indices into "positional" indices

--- modSAM2bedRMod/samtags_helper.py
+++ modSAM2bedRMod/samtags_helper.py
@@ -34,7 +34,17 @@ def scale_score_ML_tag(prob):
     return round(prob/256 * 1000)
 
 
-def read_bitflag(flag):
+def read_flag(flag):
+    """
+    Reads flag of the line and returns whether the line should be skipped or not and signalises whether the seq is
+    reverse complemented.
+    Skipping the line is set to true if mapping mistakes were recorded, multiple mappings are in the same file or
+    (non-)optical duplicates are recorded.
+    Especially the (non-)duplicate part has to be checked, as this ignores all reads marked as duplicates even though
+    at least one should be read!
+    :param flag:
+    :return: Bool(skip), Bool(reverse complement)
+    """
     skip = False
     rc = False
     # if flag == 0, do everything as before
@@ -74,6 +84,51 @@ def read_bitflag(flag):
             i += 1
 
     return skip, rc
+
+
+def single_base_to_sequence_indices(mod_df, seq):
+    """
+    The function converts the modification position in accordance to the complete read sequence.
+    The current mod_indices reflect the number of e.g. Cs until the modified C is reached (0-based counting).
+    This function adjust these "consecutive indices" (C+m, 0, 2, 1) into "positional indices" (C+m, 0, 3, 5) on the
+    read sequence.
+    I'd like to further extend this function to return the positions on the reference segment of the read, but the now
+    available test files don't contain neither reference segments nor read starting positions.
+    :param mod_df: Dataframe with "wrong" (consecutive) mod_index information of one SAM line.
+    :param seq: Read sequence of this SAM line.
+    :return: Dataframe with correct (positional) mod_index information of the SAM line.
+    """
+    # count length of sequences as well as occurrences of bases and assign correct 0-based positions to modifications
+    for j in range(len(mod_df)):
+        ref_base, strand, mod_type, abs_pos, score = mod_df.iloc[j]
+        print(ref_base, strand, abs_pos)
+        curr_counter = 0
+        A_counter = 0
+        C_counter = 0
+        G_counter = 0
+        T_counter = 0
+        for i, base in enumerate(seq):
+            if base == "A" or base == "a":
+                A_counter += 1
+                curr_counter = A_counter
+            elif base == "C" or base == "c":
+                C_counter += 1
+                curr_counter = C_counter
+            elif base == "G" or base == "g":
+                G_counter += 1
+                curr_counter = G_counter
+            elif base == "T" or base == "t":
+                T_counter += 1
+                curr_counter = T_counter
+            else:
+                curr_counter = i
+
+            if base == ref_base and curr_counter == abs_pos:
+                mod_df.loc[j, "mod_index"] = i
+            elif ref_base == "N" and i == abs_pos:  # nothing changes?
+                continue
+                # pos = i
+    return mod_df
 
 
 if __name__ == "__main__":
