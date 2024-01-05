47579919abc1b58332e5feca116e639c6116a2a5
anmabu <abusch01@students.uni-mainz.de>
2024-01-05 15:33:15 +0100

    integrate rev comp into strand

--- modSAM2bedRMod/read_SAM.py
+++ modSAM2bedRMod/read_SAM.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from Bio.Seq import Seq
+
 from modSAM2bedRMod import samtags_helper
 
 sam_file = pd.read_csv("example_files/MM-multi.sam", comment="@", delimiter="\t", header=None)
@@ -49,69 +51,50 @@ def sam2proEUF():
         mod_index = [x.split(",", 1)[1] for x in mod_list if len(x) != 0]
         print(mod_index)
 
+        # check FLAGs of line and act accordingly
+        skip, reverse_complemented = samtags_helper.read_flag(flag)
+
+        if skip:  # sequences is unmapped and/or not usable in this context
+            continue  # jump to next line in sam file
+        if reverse_complemented:
+            seq = Seq(seq).reverse_complement()
+            qual = qual[::-1]  # qual is not used as of now
+            # change reference letter in mod_type
+            mod_type = list(map(lambda x: str(Seq(x[0]).reverse_complement() + x[1:]), mod_type))
+
         # there are multiple modifications possible at each index
         # mod_number indicates how many consecutive probabilites belong to each modified type
         # determine how many symbols are after the + or - (indicating the strand)
+        # Check if after +/- are numbers (ChEBI)
         mod_number = []
         strand = []
         for mod in mod_type:
             if "+" in mod:
-                strand.append("+")
-                mod_number.append(len(mod.split("+")[1]))
-            elif "-" in mod:
-                strand.append("-")
-                mod_number.append(len(mod.split("-")[1]))
-
-        # check FLAGs of line and act accordingly
-        bit_flag = bin(flag)
-
-        # if flag == 0, do everything as before
-        if flag > 0:
-            i = 1
-            while i < len(bit_flag) - 1:
-                if i == 1 and bit_flag[-i] == "1":  # relevance
-                    print("template having multiple sequences in sequencing")
-                if i == 2 and bit_flag[-i] == "1":  # okay? i guess?
-                    print("each segment properly aligned according to the aligner")
-                if i == 3 and bit_flag[-i] == "1":
-                    print("segment unmapped")
-                    # bad
-                if i == 4 and bit_flag[-i] == "1":  # doesn't matter
-                    print("next segment in the sequence is unmapped")
-                if i == 5 and bit_flag[-i] == "1":
-                    print("Seq being reverse complemented")
-                    # take action here
-                if i == 6 and bit_flag[-i] == "1":  # doesn't matter
-                    print("Seq of next segment in template being reverse complemented")
-                if i == 7 and bit_flag[-i] == "1":  # doesn't matter
-                    print("first segment in the template")
-                if i == 8 and bit_flag[-i] == "1":  # doesn't matter
-                    print("last segment in the template")
-                if i == 9 and bit_flag[-i] == "1":  # ?
-                    print("secondary alignment")
-                if i == 10 and bit_flag[-i] == "1":  # bad!
-                    print("not passing filters such as platform/vendor quality controls")
-                if i == 11 and bit_flag[-i] == "1":  # okay?
-                    print("PCR or optical duplicate")
-                if i == 12 and bit_flag[-i] == "1":  # not a bad thing
-                    print("supplementary alignment")
-                i += 1
-
-        # change counting relative index to be consecutive
-        abs_positions = []
-        for occur in mod_index:
-            rel_positions = occur.split(",")
-            abs_positions_local = []
-            i = 0
-            while i < len(rel_positions):
-                if i == 0:
-                    abs_positions_local.append(int(rel_positions[i]) + 1)
-                    i += 1
+                if reverse_complemented:
+                    strand.append("-")
                 else:
-                    new_pos = abs_positions_local[i - 1] + int(rel_positions[i]) + 1
-                    abs_positions_local.append(new_pos)
-                    i += 1
-            abs_positions.append(abs_positions_local)
+                    strand.append("+")
+                mod_code = mod.split("+")[1]
+                if any(char.isdigit() for char in mod_code):
+                    # if there is any digit, the assumtion is that the whole code is ChEBI
+                    # I don't think something like "C+m76794" or multiple ChEBI codes concatenated are allowed
+                    mod_number.append(1)
+                else:
+                    mod_number.append(len(mod.split("+")[1]))
+            elif "-" in mod:
+                if reverse_complemented:
+                    strand.append("+")
+                else:
+                    strand.append("-")
+                mod_code = mod.split("-")[1]
+                if any(char.isdigit() for char in mod_code):
+                    mod_number.append(1)
+                else:
+                    mod_number.append(len(mod.split("-")[1]))
+
+        # change counting index, starting from 0 after finding the next modification to starting for all
+        # modifications from the first base (e.g. C)
+        abs_positions = samtags_helper.consecutive_to_positional_single_base(mod_index)
         print(abs_positions)
 
         mod_position_df = pd.DataFrame(columns=["ref_base", "strand", "mod_type", "mod_index", "score"])
@@ -136,38 +119,8 @@ def sam2proEUF():
         print(mod_position_df)
 
         # the current mod_indices reflect e.g. the number of Cs until this modified C is reached
-        # the following code adjusts the modification position in accordance to the complete read
-        # count length of sequences as well as occurrences of bases and assign correct 0-based positions to modifications
-        for j in range(len(mod_position_df)):
-            ref_base, strand, mod_type, abs_pos, score = mod_position_df.iloc[j]
-            print(ref_base, strand, abs_pos)
-            curr_counter = 0
-            A_counter = 0
-            C_counter = 0
-            G_counter = 0
-            T_counter = 0
-            for i, base in enumerate(seq):
-                if base == "A" or base == "a":
-                    A_counter += 1
-                    curr_counter = A_counter
-                elif base == "C" or base == "c":
-                    C_counter += 1
-                    curr_counter = C_counter
-                elif base == "G" or base == "g":
-                    G_counter += 1
-                    curr_counter = G_counter
-                elif base == "T" or base == "t":
-                    T_counter += 1
-                    curr_counter = T_counter
-                else:
-                    curr_counter = i
-
-                if base == ref_base and curr_counter == abs_pos:
-                    mod_position_df.loc[j, "mod_index"] = i
-                elif ref_base == "N" and i == abs_pos:  # nothing changes?
-                    continue
-                    # pos = i
-
+        # the following function adjusts the modification position in accordance to the complete sequence
+        mod_position_df = samtags_helper.single_base_to_sequence_indices(mod_position_df, seq)
         print(mod_position_df)
 
         # translate code to Abbreviation of modification
@@ -189,9 +142,6 @@ def sam2proEUF():
 
 
 if __name__ == "__main__":
-    # sam2proEUF()
-    sam_file = pd.read_csv("example_files/MM-orient.sam", comment="@", delimiter="\t", header=None)
+    sam2proEUF()
+    sam_file = pd.read_csv("example_files/MM-multi.sam", comment="@", delimiter="\t", header=None)
     qname, flag, rname, pos, mapq, cigar, rnext, pnext, tlen, seq, qual = sam_file.iloc[1, 0:11]
-    print(flag)
-    bit_flag = bin(24)
-    i = 1
