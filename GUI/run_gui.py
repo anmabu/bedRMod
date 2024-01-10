e4fafa59927b7a731e7f37051a63aeae70beda0f
anmabu <anne.busch@uni-mainz.de>
2024-01-10 10:44:11 +0100

    set default text for new config.yaml

--- GUI/run_gui.py
+++ GUI/run_gui.py
@@ -1,13 +1,49 @@
 import sys
 
 from PySide6 import QtCore, QtWidgets, QtGui
-from PySide6.QtWidgets import QLabel, QLineEdit, QFileDialog, QPushButton, QComboBox, QTextEdit, QFrame, QRadioButton
-from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QLabel, QLineEdit, QFileDialog, QPushButton, QComboBox, QTextEdit, QFrame, QRadioButton, \
+    QWidget, QVBoxLayout
+
+
+class EditorWindow(QWidget):
+    def __init__(self, file_path):
+        super().__init__()
+        EditorWindow.setWindowTitle(self, f"{file_path}")
+        self.file_path = file_path
+        self.text_edit = QTextEdit()
+        self.text_edit.setText(
+            'options:\n  modification_type: "RNA"\n  organism: 9606\n  assembly: "GRCh38"\n  annotation_source: null\n  '
+            'annotation_version: null\n  sequencing_platform: "Illumina NovaSeq 6000"\n  basecalling: null\n  '
+            'bioinformatics_workflow: "https://github.com/y9c/m6A-sacseq"\n  experiment: "2-50 ng of poly-A enriched or '
+            'ribosome RNA-depleted RNAs were fragmented and ligated, then divided in a 2:1 ratio. 2/3 of the starting '
+            'materials are labeled by MjDim1, while the remaining 1/3 serve as the untreated control. After reverse '
+            'transcription with HIV reverse transcriptase (Worthington Biochemical Corp), the cyclic allyl m6A sites '
+            'are converted to mismatches, while unconverted m6A sites in the control group are read as A."\n  '
+            'external_source: "GEO;GSE198246"\n  methods: "m6A-SAC-seq"\n  references:\n    pubmed_id: "36434097"\n \n  '
+            'modifications_file: "example_files/mod_indices.csv"')
+        self.initUI()
+
+    def initUI(self):
+        save_button = QPushButton('Save and Close', self)
+        save_button.clicked.connect(self.save_and_close)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.text_edit)
+        layout.addWidget(save_button)
+
+        self.setLayout(layout)
+
+    def save_and_close(self):
+        content = self.text_edit.toPlainText()
+        with open(self.file_path, 'w') as new_file:
+            new_file.write(content)
+        self.close()
 
 
 class MyWidget(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
+        MyWidget.setWindowTitle(self, "Convert to bedRMod")
 
         info_text = QTextEdit("some info what to do here, lorem ipsum dolor et amit")
         info_text.setFrameStyle(QFrame.Panel | QFrame.Sunken)
@@ -35,6 +71,8 @@ class MyWidget(QtWidgets.QWidget):
         # self.config_file_path.setStyleSheet("background-color: white")
         self.config_file = QPushButton("...")
         self.config_file.clicked.connect(self.select_config_file)
+        self.new_config_file = QPushButton("New Config file")
+        self.new_config_file.clicked.connect(self.create_new_file)
 
         delimiter_label = QLabel("Select file type / column delimiter")
         self.delimiter = QComboBox()
@@ -122,6 +160,10 @@ class MyWidget(QtWidgets.QWidget):
                                        "(Or do this calculation in a script and store the result in the same file)")
         self.frequency_function.setFixedHeight(line_height * 1.6)
 
+        # convert now!
+        self.convert = QPushButton()
+        self.convert.setText("Convert!")
+
         # layout stuff
         layout = QtWidgets.QGridLayout()
 
@@ -131,9 +173,10 @@ class MyWidget(QtWidgets.QWidget):
         layout.addWidget(self.input_file, 1, 2)
 
         # config file
-        layout.addWidget(config_label, 2, 0)
-        layout.addWidget(self.config_file_path, 2, 1)
-        layout.addWidget(self.config_file, 2, 2)
+        layout.addWidget(config_label, 2, 0, 1, 1)
+        layout.addWidget(self.config_file_path, 2, 1, 1, 1)
+        layout.addWidget(self.config_file, 2, 2, 1, 1)
+        layout.addWidget(self.new_config_file, 2, 3, 1, 1)
 
         layout.addWidget(delimiter_label, 3, 0)
         layout.addWidget(self.delimiter, 3, 1)
@@ -164,6 +207,8 @@ class MyWidget(QtWidgets.QWidget):
         layout.addWidget(self.frequency, 11, 1, 1, 1)
         layout.addWidget(self.frequency_function, 11, 2, 1, 1)
 
+        layout.addWidget(self.convert, 12, 0, 1, 4)
+
         self.setLayout(layout)
 
     @QtCore.Slot()
@@ -183,6 +228,23 @@ class MyWidget(QtWidgets.QWidget):
                                                    "All Files(*)")
         if pathFile:
             self.config_file_path.setText(pathFile)
+
+    @QtCore.Slot()
+    def create_new_file(self):
+        # Ask the user to choose a location and name for the new file
+        conf_file = QFileDialog()
+        conf_file.setWindowTitle("New config.yaml")
+        conf_file.setDefaultSuffix("yaml")
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+
+        file_path, _ = conf_file.getSaveFileName(self, "Save File", ".yaml", "Config Files (*.yaml);;All Files (*)",
+                                                 options=options)
+        # file_path = conf_file.exec_()
+        # If the user selected a file, create a new file
+        if file_path:
+            self.editor = EditorWindow(file_path)
+            self.editor.show()
 
 
 if __name__ == "__main__":
