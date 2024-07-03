c65ccb62291f591fe81f90ed7e2af5c4e67f7274
anmabu <anne.busch@uni-mainz.de>
2024-07-03 11:45:07 +0200

    test parse_row if it returns None

--- test/tsv2bedrmod_test.py
+++ test/tsv2bedrmod_test.py
@@ -3,8 +3,8 @@ import pandas as pd
 
 from tsv2bedRMod.tsv2bedRMod import csv2bedRMod, df2bedRMod, parse_row
 
-# def testdf2bedrmod():
-def test_parse_row():
+
+def test_parse_row_working_example():
     columns = ['chrom', 'start_col', 'end', 'name', 'score_column', 'strandedness', 
            'thick_start', 'thick_end', 'item_rgb', 'coverage_col', 'frequency_col']
     data = [
@@ -18,3 +18,15 @@ def test_parse_row():
     # print(row)
     result = parse_row(row, columnnames=df.columns, ref_seg="chrom", start="start_col", start_function=None, modi="name", modi_column=True, score="score_column", score_function=None, strand="strandedness", coverage="coverage_col", coverage_function=None, frequency="frequency_col", frequency_function=None)
     assert result == ("1", 1000, 1001, "m1A", 900, "+", 1000, 1001, "0,128,128", 30, 90)
+
+
+def test_parse_row_freq_none():
+    columns = ['chrom', 'start_col', 'end', 'name', 'score_column', 'strandedness', 'thick_start', 'thick_end',
+               'item_rgb', 'coverage_col', 'frequency_col']
+    data = [['chr1', 1000, 1001, 'm1A', 900, '+', 1000, 1001, '0,128,128', 30, 90]]
+    df = pd.DataFrame(data, columns=columns)
+    row = df.head(1).iloc[0]
+    def freq_func(param):
+        return None
+    result = parse_row(row, columnnames=df.columns, ref_seg="chrom", start="start_col", start_function=None, modi="name", modi_column=True, score="score_column", score_function=None, strand="strandedness", coverage="coverage_col", coverage_function=None, frequency="frequency_col", frequency_function=freq_func)
+    assert result is None
