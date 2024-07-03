9e52fb090fb3d23dd3bcaaa7a738bd8a16c27ea5
anmabu <anne.busch@uni-mainz.de>
2024-07-03 14:40:41 +0200

    test df2bedrmod

--- test/tsv2bedrmod_test.py
+++ test/tsv2bedrmod_test.py
@@ -7,12 +7,7 @@ from tsv2bedRMod.tsv2bedRMod import csv2bedRMod, df2bedRMod, parse_row
 def test_parse_row_working_example():
     columns = ['chrom', 'start_col', 'end', 'name', 'score_column', 'strandedness', 
            'thick_start', 'thick_end', 'item_rgb', 'coverage_col', 'frequency_col']
-    data = [
-        ['chr1', 1000, 1001, 'm1A', 900, '+', 1000, 1001, '0,128,128', 30, 90],
-        #['chr2', 2000, 2500, 'gene2', 850, '-', 2000, 2500, '0,255,0', 45, 0.8],
-        #['chr3', 3000, 3500, 'gene3', 700, '+', 3000, 3500, '0,0,255', 60, 0.9],
-        #['chr4', 4000, 4500, 'gene4', 920, '-', 4000, 4500, '255,255,0', 50, 0.7]
-            ]
+    data = [['chr1', 1000, 1001, 'm1A', 900, '+', 1000, 1001, '0,128,128', 30, 90]]
     df = pd.DataFrame(data, columns=columns)
     row = df.head(1).iloc[0]
     # print(row)
@@ -21,12 +16,33 @@ def test_parse_row_working_example():
 
 
 def test_parse_row_freq_none():
+    def freq_func(param):
+        return None
     columns = ['chrom', 'start_col', 'end', 'name', 'score_column', 'strandedness', 'thick_start', 'thick_end',
                'item_rgb', 'coverage_col', 'frequency_col']
     data = [['chr1', 1000, 1001, 'm1A', 900, '+', 1000, 1001, '0,128,128', 30, 90]]
     df = pd.DataFrame(data, columns=columns)
     row = df.head(1).iloc[0]
-    def freq_func(param):
-        return None
-    result = parse_row(row, columnnames=df.columns, ref_seg="chrom", start="start_col", start_function=None, modi="name", modi_column=True, score="score_column", score_function=None, strand="strandedness", coverage="coverage_col", coverage_function=None, frequency="frequency_col", frequency_function=freq_func)
+    result = parse_row(row, columnnames=df.columns, ref_seg="chrom", start="start_col", start_function=None,
+                       modi="name", modi_column=True, score="score_column", score_function=None, strand="strandedness",
+                       coverage="coverage_col", coverage_function=None, frequency="frequency_col",
+                       frequency_function=freq_func)
     assert result is None
+
+
+def test_df2bedrmod():
+    columns = ['chrom', 'start_col', 'end', 'name', 'score_column', 'strandedness',
+               'thick_start', 'thick_end', 'item_rgb', 'coverage_col', 'frequency_col']
+    data = [
+        ['chr1', 1000, 1001, 'm1A', 900, '+', 1000, 1001, '0,128,128', 30, 90],
+        ['2', 1001, 1002, 'm5C', 850, '-', 1001, 1002, '0,255,0', 45, 83],
+        ['chrX', 3001, 3002, 'm1A', 700, '+', 3001, 3002, '0,0,255', 6, 79],
+        ['chrY', 4001, 4002, 'm3C', 920, '-', 4001, 4002, '255,255,0', 50, 37],
+        ['chrM', 5001, 5002, 'm3C', 920, '-', 5001, 5002, '255,255,0', 23, 65]
+    ]
+
+    df = pd.DataFrame(data, columns=columns)
+    df2bedRMod(df, "test_config.yaml", "test_df2bedrmod.bedrmod", ref_seg="chrom", start="start_col",
+                        start_function=None, modi="name", modi_column=True, score="score_column", score_function=None,
+                        strand="strandedness", coverage="coverage_col", coverage_function=None,
+                        frequency="frequency_col", frequency_function=None)
