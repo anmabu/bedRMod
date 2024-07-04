4474715caa52af38e23bb7cfa18401a877653a52
anmabu <anne.busch@uni-mainz.de>
2024-07-04 12:48:00 +0200

    parsing text correctly from GUI

--- GUI/connector.py
+++ GUI/connector.py
@@ -20,22 +20,23 @@ class Connector:
         self.ui = ui
 
     def convert2bedrmod(self):
-        print(f"input file path: {self.ui.input_file_path}")
-        print(f"config yaml path: {self.ui.config_yaml_path}")
-        print(f"output file path: {self.ui.output_file_path}")
-        print(f"chrom column: {self.ui.ref_seg_column}")
-        print(f"position column: {self.ui.position_column}")
+        print(f"input file path: {self.ui.file_path.toPlainText()}")
+        print(f"config yaml path: {self.ui.config_file_path.toPlainText()}")
+        print(f"output file path: {self.ui.outfile_path.toPlainText()}")
+        print(f"chrom column: {self.ui.ref_seg.toPlainText()}")
+        print(f"position column: {self.ui.pos.toPlainText()}")
         print(f"0 indexed? {self.ui.index_0_button.isChecked()}")
         print(f"1 indexed? {self.ui.index_1_button.isChecked()}")
-        print(f"modification info: {self.ui.modification_column}")
+        print(f"modification info: {self.ui.modi.toPlainText()}")
         print(f"modification column? {self.ui.modi_button.isChecked()}")
-        print(f"strand column {self.ui.strand_column}")
-        print(f"score column {self.ui.score_column}")
-        print(f"coverage column: {self.ui.coverage_column}")
-        print(f"frequency column: {self.ui.frequency_column}")
+        print(f"strand column {self.ui.strand.toPlainText()}")
+        print(f"score column {self.ui.score.toPlainText()}")
+        print(f"coverage column: {self.ui.coverage.toPlainText()}")
+        print(f"frequency column: {self.ui.frequency.toPlainText()}")
         print(f"frequency function: {self.ui.frequency_function.toPlainText()}")
         print(f"score function: {self.ui.score_function.toPlainText()}")
         print(f"coverage function: {self.ui.coverage_function.toPlainText()}")
+        print(f"delimiter: {self.ui.delimiter.currentData()}")
         self.ui.start_func = None
         self.ui.score_func = None
         self.ui.coverage_func = None
@@ -45,3 +46,8 @@ class Connector:
 
         parsed_func = funcify("x * 2")
         print(parsed_func(3))
+
+        csv2bedRMod(self.ui.file_path, self.ui.config_file_path, self.ui.outfile_path, self.ui.delimiter,
+                    self.ui.ref_seg_column, self.ui.position_column, self.ui.start_func, self.ui.modification_column,
+                    self.ui.modi_button.isChecked(), self.ui.score_column, self.ui.score_func, self.ui.strand_column,
+                    self.ui.coverage_column, self.ui.coverage_func, self.ui.frequency_column, self.ui.frequency_func)
