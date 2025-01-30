3430e388251cb7521b44d321a72e3d728e45fb74
anmabu <anne.busch@uni-mainz.de>
2025-01-30 12:59:04 +0100

    exposed functions to be called directly

--- bedRMod/__init__.py
+++ bedRMod/__init__.py
@@ -1,1 +1,5 @@
-from .convert2bedRMod import df2bedRMod, csv2bedRMod
+__version__ = "1.8.0-rc2"
+
+from .convert2bedRMod import df2bedRMod, csv2bedRMod, parse_row
+from .read import read_header, read_data, read_bedRMod
+from .write import write_header, write_data, write_bedRMod
