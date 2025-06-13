92dd561ff8c4e1075901d06aa302fbe6fd77eece
anmabu <anne.busch@uni-mainz.de>
2025-06-13 16:48:49 +0200

    test read_single_data_row

--- test/read_test.py
+++ test/read_test.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from pathlib import Path
 
-from bedRMod import read_header, read_data_to_df, read_bedRMod
+from bedRMod import read_header, read_data_to_df, read_single_data_row, read_bedRMod
 
 test_dir = Path(__file__).parent
 
@@ -12,7 +12,7 @@ def test_read_header():
     assert header == {'fileformat': 'bedRModv1.8', 'organism': '9606', 'modification_type': 'RNA', 'assembly': 'GRCh38', 'annotation_source': 'Ensemble', 'annotation_version': '93', 'sequencing_platform': 'Illumina NovaSeq 6000', 'basecalling': '', 'bioinformatics_workflow': 'workflow:https://github.com/anmabu/bedRMod;coverage_function:round(coverage);frequency_function:round(frequency);score_function:round(-log10(score))', 'experiment': 'https://doi.org/test', 'external_source': 'GEO;GSETEST', 'methods': 'TEST', 'references': 'pubmed_id:12345678', 'conversion_information': 'rgb value in df is ignored and calculated directly'}
 
 
-def test_read_data():
+def test_read_data_to_df():
     test_file = f"{test_dir}/test_df2bedrmod.bedrmod"
     columns = ["chrom", "chromStart", "chromEnd", "name", "score", "strand", "thickStart", "thickEnd", "itemRgb", "coverage", "frequency"]
     data = [
@@ -29,3 +29,8 @@ def test_read_data():
     df_diff = data.compare(compare_df)
     assert df_diff.empty
 
+def test_read_single_data_row():
+    test_file = f"{test_dir}/test_df2bedrmod.bedrmod"
+
+    assert (read_single_data_row(test_file, 1)
+            == ['2', '1001', '1002', 'm5C', '19', '-', '1001', '1002', '0,139,139', '45', '83'])
