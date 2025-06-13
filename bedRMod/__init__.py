7dfcdfa8f87b58a8e474f8744ce476dc12ab3ce2
anmabu <anne.busch@uni-mainz.de>
2025-06-13 16:07:33 +0200

    export function to write single data row

--- bedRMod/__init__.py
+++ bedRMod/__init__.py
@@ -3,4 +3,4 @@ __version__ = "1.8.0-rc3"
 from .convert2bedRMod import df2bedRMod, csv2bedRMod
 from .helper import parse_row
 from .read import read_header, read_data, read_bedRMod
-from .write import write_header_from_dict, write_header_from_config,  write_data_from_df, write_bedRMod
+from .write import write_header_from_dict, write_header_from_config, write_data_from_df, write_single_data_row, write_bedRMod
