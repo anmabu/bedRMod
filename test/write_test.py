105db7685eb397b7433d66ce335b60ba2e244ebd
anmabu <anne.busch@uni-mainz.de>
2025-06-13 16:08:55 +0200

    test_write_header_from_config

--- test/write_test.py
+++ test/write_test.py
@@ -1,19 +1,29 @@
 import pandas as pd
 from pathlib import Path
+from ruamel.yaml import YAML
 
-from bedRMod import write_header_from_dict, write_data_from_df
+yaml = YAML()
+yaml.sort_base_mapping_type_on_output = False  # disable sorting of keys
+
+from bedRMod import write_header_from_dict, write_header_from_config, write_data_from_df, write_single_data_row
 from bedRMod import read_header, read_data
 
 test_dir = Path(__file__).parent
 
-def test_write_header():
+def test_write_header_from_dict():
     header_dict = read_header(f"{test_dir}/test_df2bedrmod.bedrmod")
-    write_header_from_dict(f"{test_dir}/test_write2bedrmod.bedrmod", header_dict)
-
+    write_header_from_dict(header_dict, f"{test_dir}/test_write2bedrmod.bedrmod")
     assert header_dict == read_header(f"{test_dir}/test_write2bedrmod.bedrmod")
 
-def test_write_data():
+def test_write_data_from_df():
     data_df = read_data(f"{test_dir}/test_df2bedrmod.bedrmod")
-    write_data_from_df(f"{test_dir}/test_write2bedrmod.bedrmod", data_df)
+    write_data_from_df(data_df, f"{test_dir}/test_write2bedrmod.bedrmod")
     diff_df = data_df.compare(read_data(f"{test_dir}/test_write2bedrmod.bedrmod"))
     assert diff_df.empty
+
+def test_write_header_from_config():
+    write_header_from_config(f"{test_dir}/test_config.yaml", f"{test_dir}/test_output_config.bedrmod")
+    assert read_header(f"{test_dir}/test_write2bedrmod.bedrmod") == read_header(f"{test_dir}/test_output_config.bedrmod")
+
+def test_write_single_data_row():
+    pass
